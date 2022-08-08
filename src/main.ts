a1f2d50f200cbf9a0c9ba58d9fbcf081b0032bfd
Brandon <brandon.lange@aboutyou.com>
2022-08-08 23:18:27 +0200

    added cors for development + validate route in auth controller

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({
+      origin: 'http://localhost:8080',
+    });
+  }
+
   await app.listen(3000);
 }
 bootstrap();
