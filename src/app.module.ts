868cb301d8995d2d908a09791f00c2daeb088b00
Brandon <brandon.lange@aboutyou.com>
2022-08-08 06:47:03 +0200

    added user module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 import CONFIG_MODULE from './config/dotenv';
 import DB_MODULE from './config/typeorm';
 
 @Module({
-  imports: [CONFIG_MODULE, DB_MODULE],
+  imports: [CONFIG_MODULE, DB_MODULE, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
