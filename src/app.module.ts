ab299173457eac835033a12d2ce8162683d0156a
Brandon <brandon.lange@aboutyou.com>
2022-08-09 08:39:10 +0200

    added sendgrid and event for user registration email confirmation

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,23 @@ import { HashModule } from './hash/hash.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { ProjectsModule } from './projects/projects.module';
 import { EmailModule } from './email/email.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 import CONFIG_MODULE from './config/dotenv';
 import DB_MODULE from './config/typeorm';
 
 @Module({
-  imports: [CONFIG_MODULE, DB_MODULE, UsersModule, AuthModule, HashModule, SuppliersModule, ProjectsModule, EmailModule],
+  imports: [
+    CONFIG_MODULE,
+    DB_MODULE,
+    EventEmitterModule.forRoot(),
+    UsersModule,
+    AuthModule,
+    HashModule,
+    SuppliersModule,
+    ProjectsModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import * as sgMail from '@sendgrid/mail';
+import { UserRegisteredEvent } from 'src/users/events/user-registered.event';
+
+interface MailMessage {
+  to: string;
+  from: string;
+  subject: string;
+  html: string;
+}
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private canSend: boolean = false;
+
+  constructor() {
+    if (process.env.SENDGRID_API_KEY !== '') {
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      this.canSend = true;
+    }
+  }
+
+  private async send(message: MailMessage) {
+    const retval = {
+      success: true,
+    };
+
+    if (!this.canSend) {
+      console.log('Mock send mail!');
+      return retval;
+    }
+
+    try {
+      const mailResponse = await sgMail.send(message);
+    } catch (err) {
+      retval.success = false;
+    }
+
+    return retval;
+  }
+
+  @OnEvent('user.registered')
+  async sendPasswordConfirmationMail(event: UserRegisteredEvent) {
+    const msg = {
+      to: event.emailAddress,
+      from: 'brandonlostboy@gmail.com',
+      subject: 'Confirm your email',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
+    const mailResponse = await this.send(msg);
+
+    if (!mailResponse.success) {
+      console.error(
+        'There was an issue sending the password confirmation mail',
+      );
+    }
+  }
+}
